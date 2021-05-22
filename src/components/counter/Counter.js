aa6f8161436aaed82008d2b2309e46c4649e4c86
David uribe <Daviduribe0007@gmail.com>
2021-05-22 18:59:17 -0500

    Add the funcionality with states to increment and decrement the counter

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 import './Counter.css';
-export default function Counter() {
-  return (
-    <div className="counter">
-      <td class="tableOperations">
-        <tr>
-          <button class="incrementMount">+1</button>
-          <button class="decrementMount">-1</button>
-        </tr>
-        <tr>
-          <span class="resoult"> ff</span>
-        </tr>
-      </td>
-    </div>
-  );
+
+class Counter extends Component {
+  //Define ethe inicial state in a constructor
+  //state-> counter 0
+  constructor() {
+    super();// you need call this super method
+    this.state = {
+      counter: 0
+    }
+    this.incrementOne = this.incrementOne.bind(this); 
+    this.decrementOne = this.decrementOne.bind(this); // bind the method incrementMount
+  }
+  render() {
+    return (
+      <div className="counter">
+        <td class="tableOperations">
+          <tr>
+            <button class="incrementButton" onClick={this.incrementOne}>+1</button>
+            <button class="decrementButton"onClick={this.decrementOne}>-1</button>
+          </tr>
+          <tr>
+            <span class="resoult"> {this.state.counter}</span>
+          </tr>
+        </td>
+      </div>
+    );
+  }
+  incrementOne() {
+    //console.log('ingffff');
+    this.setState({
+      counter: this.state.counter += 1
+    });
+  
+  }
+  decrementOne() {   
+    this.setState({
+      counter: this.state.counter -= 1
+    });
+  
+  }
+
 }
+
+export default Counter;
 
 /*or you can use only one fuction and export like this->
 function  Counter() {
