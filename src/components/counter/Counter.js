62e5cd5a3658d6048828e3a8cc72228ab9f2b106
David uribe <Daviduribe0007@gmail.com>
2021-05-22 17:13:41 -0500

    cretae the buttoms and add the colors on css

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
- export  default function  Counter() {
-    return (
-      <div className="counter">
-      this is the counter
-      </div>
-    );
-  }
+import './Counter.css';
+export default function Counter() {
+  return (
+    <div className="counter">
+      <td class="tableOperations">
+        <tr>
+          <button class="incrementMount">+1</button>
+          <button class="decrementMount">-1</button>
+        </tr>
+        <tr>
+          <span class="resoult"> ff</span>
+        </tr>
+      </td>
+    </div>
+  );
+}
 
-  /*or you can use only one fuction and export like this->
-  function  Counter() {
-    return (
-      <div className="counter">
-      this is the counter
-      </div>
-    );
-  }
-  export default Counter //this is importan to import only a function 
-  */
+/*or you can use only one fuction and export like this->
+function  Counter() {
+  return (
+    <div className="counter">
+    this is the counter
+    </div>
+  );
+}
+export default Counter //this is importan to import only a function
+*/
